7c52882df7642f3b885f6198a726a9dd6770dc5c
NaimaAmraan <140434438+NaimaAmraan@users.noreply.github.com>
2025-03-04 10:07:50 -0800

    environ.get -> os.getenv (api key issues)

--- main.py
+++ main.py
@@ -18,8 +18,8 @@ from supabase import create_client, Client
 load_dotenv()
 
 
-url: str = os.environ.get("SUPABASE_URL")
-key: str = os.environ.get("SUPABASE_ANON_KEY")
+url: str = os.getenv("SUPABASE_URL")
+key: str = os.getenv("SUPABASE_ANON_KEY")
 supabase: Client = create_client(url, key)
 
 # File paths
